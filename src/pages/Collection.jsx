a59916512a3788fc0c312704b0a49d825de28aa2
Alfushaonline <nidhommaulana9@gmail.com>
2022-11-20 11:56:15 +0700

    update

---
+++ src/pages/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DaftarBarang from '../components/DaftarBarang'
+
+function Collection() {
+  return (
+    <div className="collection" id="collection">
+      <DaftarBarang />
+    </div>
+  );
+}
+
+export default Collection
